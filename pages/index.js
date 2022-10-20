785e5aace13c26774e21bc1921004eb38d4d231a
Napat <omegyzr@gmail.com>
2022-10-20 17:33:50 +0700

    add background animation

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Sadboi-Web</title>
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.title}>CPE Sadboi Profile</div>
-        <div className={styles.mainli}>
-          <Link href="/niwatchai"><li><a>63070501025 Niwatchai Wangtrakuldee</a></li></Link>
-          <Link href="/niwatchai"><li><a>63070501025 Niwatchai Wangtrakuldee</a></li></Link>
-          <Link href="/niwatchai"><li><a>63070501025 Niwatchai Wangtrakuldee</a></li></Link>
-        </div>
-      </main>
+    <div className={styles.area}>
+        <Head>
+          <title>Sadboi-Web</title>
+        </Head>
+        <main className={styles.main}>
+          <div className={styles.title}>CPE Sadboi Profile</div>
+          <div className={styles.mainli}>
+            <Link href="https://github.com/Napat-Edu">
+              <li>
+                <a>Napat Wareedee</a>
+              </li>
+            </Link>
+          </div>
+        </main>
 
-      <footer className={styles.footer}>
-        <h4>Sadboi CPE34</h4> &nbsp;Copyright &copy; 2022  
-      </footer>
-  </div>
-  )
-};
+        <ul className={styles.circles}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <footer className={styles.footer}>
+          <h4>Sadboi CPE34</h4> &nbsp;Copyright &copy; 2022
+        </footer>
+    </div>
+  );
+}
